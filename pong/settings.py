9398124ed014a0f598e9de3087e3b0a8fdfa191e
Theouche <theo.lorne@gmail.com>
2024-08-27 15:43:19 +0200

    still big probleme

--- pong/settings.py
+++ pong/settings.py
@@ -134,3 +134,20 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
